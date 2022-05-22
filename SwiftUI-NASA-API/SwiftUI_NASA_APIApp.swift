b73d184fd559950258f6a4187e98ff478f61c9c5
francobuena <francobuena@outlook.com>
2022-05-22 16:00:11 +1000

    Initial Commit

---
+++ SwiftUI-NASA-API/SwiftUI_NASA_APIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_NASA_APIApp.swift
+//  SwiftUI-NASA-API
+//
+//  Created by Buena, Franco on 22/5/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_NASA_APIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
